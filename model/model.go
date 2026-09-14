13a7a82552dc770157582601cd868eaef2e259ae
MJ-9527 <3285501430@qq.com>
2026-09-14 10:19:55 +0800

    测试git

--- model/model.go
+++ model/model.go
@@ -78,10 +78,16 @@ type RegisterToolRequest struct {
 	Name        string          `json:"name" binding:"required,max=100"`
 	Description string          `json:"description" binding:"max=1000"`
 	Category    string          `json:"category" binding:"required,max=100"`
-	Tags        []string        `json:"tags"`
 	InputSchema json.RawMessage `json:"input_schema" binding:"required"`
 	Version     string          `json:"version" binding:"required,max=50"`
 }
+
+type GrantToolPermissionRequest struct {
+	UserID *int64  `json:"user_id"`
+	RoleID *int64  `json:"role_id"`
+	Action *string `json:"action"`
+}
+
 type ToolVersion struct {
 	ID          int64           `json:"id" db:"id"`
 	ToolID      int64           `json:"tool_id" db:"tool_id"`
